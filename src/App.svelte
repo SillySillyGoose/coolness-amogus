982d01ee748ad77b8628f6b4f25a8f19076a3b0e
SillySillyGoose <mark.hop@hotmail.com>
2023-10-02 10:12:39 +1100

    Removed The Heart, Broke.

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,10 @@
-<script>
-	let editP = document.getElementById("P0");
-	let EDB = document.querySelector("#EDB");
-	let PVal = 0;
 
-		if (localStorage.getItem("PValStore"))
-			{
-				PVal = localStorage.getItem("PValStore")
-			}
-		else
-			{
-				PVal = 0
-			}
-
-
-
-
-	function PVAL1() {
-		PVal = PVal + 1
-		localStorage.setItem("PValStore", PVal)
-	}
-
-
-
-
-</script>
 
 <div>
-
 	<h1 style="font-family: Arial;">
 
 			<strong>1 DAY</strong>
-			<a id="EDB" href="#top" on:click={PVAL1}>
-			<img style="width: 52px; padding-left: 20px;" src="https://cdn.iconscout.com/icon/free/png-256/free-heart-like-love-card-game-favorite-37955.png?f=webp&w=256" alt="heart">
-			</a>
 			<br>
-			<p style="padding-left: 125px;" id="P0">{PVal}</p>
 
 	</h1>
 
