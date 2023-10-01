fc428121bbc02569e09d74f946d249018b9a9ff7
SillySillyGoose <mark.hop@hotmail.com>
2023-10-02 09:43:49 +1100

    Added A Clickable Heart to show your support!

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,26 @@
+<script>
+	let editP = document.getElementById("P0")
+	let EDB = document.getElementById("EDB")
+	let PVal = localStorage.getItem("PValStore");
+
+	EDB.addEventListener("click", () => {
+		PVal += 1
+		localStorage.setItem("PValStore", PVal)
+	})
+
+
+</script>
+
 <div>
 
 	<h1 style="font-family: Arial;">
 
 			<strong>1 DAY</strong>
+			<a id="EDB" href="#top">
 			<img style="width: 52px; padding-left: 20px;" src="https://cdn.iconscout.com/icon/free/png-256/free-heart-like-love-card-game-favorite-37955.png?f=webp&w=256" alt="heart">
+			</a>
 			<br>
-			<p style="margin-right: 20px;">0</p>
+			<p style="margin-right: 20px;" id="P0">{PVal}</p>
 
 	</h1>
 
