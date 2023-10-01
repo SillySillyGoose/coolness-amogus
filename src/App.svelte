e872df95781f011cdbcd65cd2cffbddcb6c9ab6d
SillySillyGoose <mark.hop@hotmail.com>
2023-10-02 10:14:19 +1100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,19 @@
 <div>
 	<h1 style="font-family: Arial;">
 
-			<strong>1 DAY</strong>
+			<strong>10 DAYS</strong>
 			<br>
+
+
+			<footer>
+				<h1>
+					Whats New?
+					<br>
+					"Account Creation (Maybe)"
+
+
+				</h1>
+			</footer>
 
 	</h1>
 
