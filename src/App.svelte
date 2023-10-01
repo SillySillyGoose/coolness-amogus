66cf156cb05a72a67f77686e7c27bcd9b5097385
SillySillyGoose <mark.hop@hotmail.com>
2023-10-02 10:02:36 +1100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
 	let editP = document.getElementById("P0");
 	let EDB = document.querySelector("#EDB");
-	let PVal;
+	let PVal
 
 		if (localStorage.getItem("PValStore"))
 			{
@@ -31,10 +31,10 @@
 
 			<strong>1 DAY</strong>
 			<a id="EDB" href="#top" on:click={PVAL1}>
-			<img style="width: 52px; padding-right: 135px;" src="https://cdn.iconscout.com/icon/free/png-256/free-heart-like-love-card-game-favorite-37955.png?f=webp&w=256" alt="heart">
+			<img style="width: 52px; padding-left: 20px;" src="https://cdn.iconscout.com/icon/free/png-256/free-heart-like-love-card-game-favorite-37955.png?f=webp&w=256" alt="heart">
 			</a>
 			<br>
-			<p style="margin-right: 20px;" id="P0">{PVal}</p>
+			<p style="padding-left: 135px;" id="P0">{PVal}</p>
 
 	</h1>
 
