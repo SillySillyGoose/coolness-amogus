ecd561fe4c083f1917195edb2230ed0ddd62323d
SillySillyGoose <mark.hop@hotmail.com>
2023-10-02 09:22:46 +1100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,8 @@
 
 	<h1 style="font-family: Arial;">
 
-			<strong> DAY</strong>
-			<img src="https://cdn.iconscout.com/icon/free/png-256/free-heart-like-love-card-game-favorite-37955.png?f=webp&w=256" alt="heart">
+			<strong>1 DAY</strong>
+			<img style="width: 52px;" src="https://cdn.iconscout.com/icon/free/png-256/free-heart-like-love-card-game-favorite-37955.png?f=webp&w=256" alt="heart">
 
 	</h1>
 
